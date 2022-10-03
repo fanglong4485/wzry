259fbe696663b607a1e43835469652bb7f6fa3fe
fanglong4485 <fanglong4485@qq.com>
2022-10-03 22:32:05 +0800

    批量添加蓝色小兵并自动移动。

--- src/main/java/com/fl/wzry/GameFrame.java
+++ src/main/java/com/fl/wzry/GameFrame.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameFrame extends JFrame {
     private int windowWidth = 600;
@@ -22,6 +24,13 @@ public class GameFrame extends JFrame {
     MinionBlue minionBlue = new MinionBlue(this);
     MinionRed minionRed = new MinionRed(this);
 
+    /**
+     * 游戏元素列表
+     */
+    List<GameObject> objList = new ArrayList<>();
+    List<GameObject> redList = new ArrayList<>();
+    List<GameObject> blueList = new ArrayList<>();
+
     //双缓冲用，防止闪烁
     private Image offScreenImage = null;
 
@@ -36,7 +45,17 @@ public class GameFrame extends JFrame {
         setVisible(true);
 
         this.addKeyListener(new GameFrame.KeyMonitor());
+
+        //添加游戏元素
+        objList.add(bg);
+        objList.add(player);
+
+
+
         while (true) {
+            minionBlue.createMinion(this,blueList);
+            //红色同理
+            //minionBlue.createMinion(this,blueList);
             repaint();
             try {
                 Thread.sleep(25);
@@ -55,9 +74,13 @@ public class GameFrame extends JFrame {
             offScreenImage = this.createImage(1600,1200);
         }
         Graphics oImage = offScreenImage.getGraphics();
-        bg.pantSelf(oImage);
-        player.pantSelf(oImage);
-        minionBlue.pantSelf(oImage);
+
+        for (GameObject gameObject : objList) {
+            gameObject.pantSelf(oImage);
+        }
+        //bg.pantSelf(oImage);
+        //player.pantSelf(oImage);
+        //minionBlue.pantSelf(oImage);
         //minionRed.pantSelf(oImage);
         //是英雄固定在屏幕中间
         g.drawImage(offScreenImage,-player.getX() + 300,-player.getY() + 200,null);
--- src/main/java/com/fl/wzry/Minion.java
+++ src/main/java/com/fl/wzry/Minion.java
@@ -1,13 +1,70 @@
 package com.fl.wzry;
 
 import java.awt.*;
+import java.util.List;
 
 public abstract class Minion extends GameObject{
+
+    private boolean nextMinion = true;
+    private boolean nextLine = true;
+    private int minionCount = 0;
+
     public Minion(GameFrame gameFrame) {
         super(gameFrame);
     }
 
     public abstract void move();
+
+    class NextMinion extends Thread{
+        public void run(){
+            nextMinion = false;
+            try {
+                Thread.sleep(1500);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            nextMinion = true;
+            this.interrupt();
+        }
+    }
+
+    class NextLine extends Thread{
+        public void run(){
+            nextLine = false;
+            try {
+                Thread.sleep(5000);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            nextLine = true;
+            this.interrupt();
+        }
+    }
+
+    public void createMinion(GameFrame gameFrame, List<GameObject> minionList){
+        if (nextLine){
+            if (nextMinion){
+                if (minionList == gameFrame.blueList){
+                    //MinionBlue minionBlue = new MinionBlue(gameFrame);
+                    MinionBlue minionBlue = new MinionBlue(gameFrame);
+                    gameFrame.objList.add(minionBlue);
+                    minionList.add(minionBlue);
+                }
+                //红色小兵同理
+                //if (minionList == this.gameFrame.objList){
+                //    MinionBlue minionBlue = new MinionBlue(gameFrame);
+                //    gameFrame.objList.add(minionBlue);
+                //    minionList.add(minionBlue);
+                //}
+                minionCount ++;
+                new NextMinion().start();
+            }
+            if (minionCount == 3){
+                minionCount = 0;
+                new NextLine().start();
+            }
+        }
+    }
 
     @Override
     public Rectangle getRec() {
