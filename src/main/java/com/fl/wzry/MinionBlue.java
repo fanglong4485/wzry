32009163908bedc8704cc0293f1fae5c3bf0d44e
fanglong4485 <fanglong4485@qq.com>
2022-10-05 15:26:07 +0800

    蓝兵出生点和路线调整。

--- src/main/java/com/fl/wzry/MinionBlue.java
+++ src/main/java/com/fl/wzry/MinionBlue.java
@@ -4,24 +4,26 @@ public class MinionBlue extends Minion {
     public MinionBlue(GameFrame gameFrame) {
         super(gameFrame);
         setImage("src/main/resources/com/fl/wzry/static/bing-blue.jpg");
-        setX(235);
+        //出生点
+        setX(300);
         setY(1000);
-        setSpd(25);
+        setSpd(5);
     }
 
     @Override
     public void move() {
-        if (getX()< 1200){
-            setSpd(5);
+        //325.1025  1200.1025  1375.950 1375.350
+        if (getX()< 1200 ){
+            //setSpd(5);
             setX(getX() + getSpd());
         }
-        else if (getX() >= 1200 && getX()< 1350){
+        else if (getX() >= 1200 && getX() < 1350){
             setSpd(2);
             setX(getX() + getSpd());
             setY(getY() - getSpd());
         }
-        if (getY() >= 875 ){
-            setSpd(5);
+        else if (getX() >= 1350){
+            setSpd(2);
             setY(getY() - getSpd());
         }
     }
